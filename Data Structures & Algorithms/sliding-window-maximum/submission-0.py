4613f83eb2026cfbce81ee700330451ebc324406
AlpayAltuntas <alpaycanaltuntas@gmail.com>
2026-07-25 21:41:38 +0200

    Add: sliding-window-maximum - submission-0

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        # start doing subsets
+        # look for max in subset -> store in hashmap
+        hashmap = {}
+        res = []
+        currMax = 0
+        for i in range(0, len(nums) - k + 1):
+            currList = nums[i:i+k]
+            currMax = max(currList)
+            res.append(currMax)
+        return res
+        
