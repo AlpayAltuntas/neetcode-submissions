45db48ea6bd523f85619e179ef3079e638673518
AlpayAltuntas <alpaycanaltuntas@gmail.com>
2026-08-04 17:40:12 +0200

    Add: balanced-binary-tree - submission-1

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True
+
+        l = self.getHeight(root.left)
+        r = self.getHeight(root.right)
+
+        return (
+            abs(l - r) <= 1
+            and self.isBalanced(root.left)
+            and self.isBalanced(root.right)
+        )
+
+    def getHeight(self, node):
+        if node is None:
+            return 0
+        return 1 + max(self.getHeight(node.left), self.getHeight(node.right))
