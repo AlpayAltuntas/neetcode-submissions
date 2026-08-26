cc07ecb6c7790b5ee1f699d2e3cb0656ee1f10c6
Alpay Can Altuntas <alpaycanaltuntas@gmail.com>
2026-08-26 20:57:05 +0200

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res, subset = [], []
+        used = [False] * len(nums)
+
+        def dfs():
+            # base case
+            if len(subset) == len(nums):
+                res.append(subset.copy())
+                return
+            # Go through items
+            for i in range(len(nums)):
+                if used[i]: continue
+                used[i] = True
+                subset.append(nums[i])
+                dfs()
+                subset.pop()
+                used[i] = False
+
+        dfs()
+        return res
